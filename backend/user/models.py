ac1ca6cd7a7803440d96311096445b84ef473cb9
Varun-2503 <varunsubramanian2503@gmail.com>
2023-11-09 19:44:37 +0530

    Minor changes are made

--- backend/user/models.py
+++ backend/user/models.py
@@ -21,7 +21,7 @@ class AppUserManager(BaseUserManager):
             raise ValueError('An email is required.')
         if not password:
             raise ValueError('A password is required.')
-        user = self.create_user(email, password)
+        user = self.create_user(email=email, password=password)
         user.is_superuser = True
         user.save()
         return user
